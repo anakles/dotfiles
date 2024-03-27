cffc42110b6e025bed57bda15f48849d0cf4e50a
Anakles <41325371+anakles@users.noreply.github.com>
2024-03-27 21:52:54 +0100

    Updated fish shell greeting

--- iusearchbtw/.config/fish/config.fish
+++ iusearchbtw/.config/fish/config.fish
@@ -9,10 +9,30 @@ status --is-login; and begin
 
 end
 
+function fish_greeting
+    source ~/.config/rdy/vars.fish
+
+    echo -e "\n = Hello anakles! =\n"
+
+    # Case: all repos are clean
+    if test "$RDY_REPOS_DIRTY" = 0
+        echo -e "Your repos are up to date :)"
+    end
+    # Case: You have unpulled repos
+    if test "$RDY_REPOS_PULL" != 0
+        echo -e 'You have unpulled changes. Make sure to run "rdy".'
+    end
+    # Case: You have uncommited or unpushed changes
+    if test "$RDY_REPOS_PUSH" != 0 -o "$RDY_REPOS_COMMIT" != 0
+        echo -e 'You have uncommitted / unpushed changes. Make sure to save your work.'
+    end
+
+    echo -e "\n"
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -g fish_greeting
+    # set -g fish_greeting
 
     # General Commands
     abbr --add -- v nvim
@@ -25,6 +45,9 @@ if status is-interactive
     set -gx XDG_TOOLS_DIR /home/anakles/Repos/OSCPasting
 
     set -gx SSH_AUTH_SOCK /run/user/1000/ssh-agent.socket
+
+    # Color Scheme:
+    set -gx GTK_THEME Adwaita:dark
 end
 
 
