339e0be767d89da20086a2fbbed0c09fdf0c0148
NBetz <niklas.betz@usd.de>
2024-04-25 13:13:27 +0200

    feat: switched to Alacritty

--- UbuntuHost/.config/i3/scripts/toggleTheme.sh
+++ UbuntuHost/.config/i3/scripts/toggleTheme.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env sh
 set -euo
 
+ALACRITTYCONF="/home/nbetz/.config/alacritty/alacritty.toml"
+
+LIGHTTHEME="catppuccin-latte.toml"
+DARKTHEME="catppuccin-frappe.toml"
+
+sed -i "s/${LIGHTTHEME}/${DARKTHEME}/" "$ALACRITTYCONF"
+
 if test "$(gsettings get org.gnome.desktop.interface color-scheme)" = "'prefer-light'"; then
-  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-  gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+	gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+	gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+	sed -i "s/${LIGHTTHEME}/${DARKTHEME}/" "$ALACRITTYCONF"
 else
-  gsettings set org.gnome.desktop.interface color-scheme prefer-light
-  gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
+	gsettings set org.gnome.desktop.interface color-scheme prefer-light
+	gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
+
+	sed -i "s/${DARKTHEME}/${LIGHTTHEME}/" "$ALACRITTYCONF"
 fi
